d0bc3c02f86c525156f5c685f6ce0b5f3730c9f7
Raph <raphael.mercie@epitech.eu>
2022-03-07 17:31:11 +0100

    Update version

--- epitech_gen.sh
+++ epitech_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.3.1
+VERSION=0.3.2
 
 NC='\033[0m' # No Color
 RED='\033[0;31m'
--- install_epitech_gen.sh
+++ install_epitech_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.3.1
+VERSION=0.3.2
 
 if [[ $EUID -ne 0 ]]; then
     echo "The installation must be run as root."
